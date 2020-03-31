a249bae2b48abb0e2ffcf379b2e084a9bec7cee2
BadKoldrysh <ohmygodkir17@gmail.com>
2020-03-31 20:14:25 +0200

    create a global style file; modify header with using the style.css

--- public/includes/header.php
+++ public/includes/header.php
@@ -14,25 +14,24 @@ if (isset($_SESSION['username'])) {
 
 <html>
 <head>
+    <!-- META -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- JS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="/../assets/js/bootstrap.js"></script>
+    
+    <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="/../assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="/../assets/css/style.css">
+    
     <title>A Social Network Web</title>
 </head>
 <body>
-<br />
-<button type="button" class="btn btn-primary">Hello</button>
-<br />
-<div class="dropdown">
-  <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown button
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-  </div>
-</div>
+    <div class="top_bar">
+        <div class="logo">
+            <a href="index.php">Social Network</a>
+        </div>
+    </div>
