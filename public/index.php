4bee7d4ecb868db14731ea578255b82795a1d9ab
BadKoldrysh <ohmygodkir17@gmail.com>
2020-04-03 20:28:10 +0200

    create logout handler; add logout button; moidfy Post and User classes; modify index

--- public/index.php
+++ public/index.php
@@ -4,7 +4,14 @@ declare(strict_types = 1);
 
 require_once(__DIR__ . '/includes/header.php');
 require_once(__DIR__ . '/includes/classes/User.php');
+require_once(__DIR__ . '/includes/classes/Post.php');
 // session_destroy();
+
+if (isset($_POST['post'])) {
+    $post = new Post($con, $userLoggedIn);
+    $post->submitPost($_POST['post_text'], 'none');
+    header("Location: index.php");
+}
 ?>
 
         <div class="user-details column">
@@ -26,7 +33,7 @@ require_once(__DIR__ . '/includes/classes/User.php');
         </div>
 
         <div class="column main-column">
-            <form action="index.php" class="post-form">
+            <form action="index.php" class="post-form" method="post">
                 <textarea name="post_text" id="post_text" placeholder="Got something to say?"></textarea>
                 <input type="submit" name="post" id="post_button" value="Post">
                 <hr />
