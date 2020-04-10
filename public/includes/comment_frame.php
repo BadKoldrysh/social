c9e54341540bcc69420a7cd81caeb8a2429903b8
BadKoldrysh <ohmygodkir17@gmail.com>
2020-04-10 12:50:58 +0200

    create the comment frame

---
+++ public/includes/comment_frame.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types = 1);
+?>
+
+<html lang="en">
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="/../assets/css/style.css">
+</head>
+<body>
+    <?php
+    require_once(__DIR__ . '/../config/config.php');
+    require_once(__DIR__ . '/includes/classes/User.php');
+    require_once(__DIR__ . '/includes/classes/Post.php');
+
+    if (isset($_SESSION['username'])) {
+        $userLoggedIn = $_SESSION['username']; 
+        $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+        $user = mysqli_fetch_array($user_details_query);
+    } else {
+        header("Location: register.php");
+    }
+    ?>
+
+    <script>
+        function toggle() {
+            let element = document.getElementById("comment_section");
+
+            if (element.style.display == "block") {
+                element.style.display = "none";
+            } else {
+                element.style.display = "block";
+            }
+        }
+    </script>
+    
+    <?php
+    // get id of post
+    if (isset(($_GET['post_id']))) {
+        $post_id = $_GET['post_id'];
+        
+    }
+
+    $user_query = mysqli_query($con, "SELECT added_by, user_to FROM posts WHERE id='$post_id'");
+    $row = mysqli_fetch_array($user_query);
+
+    $posted_to = $row['added_by'];
+
+    if (isset($_POST['postComment' . $post_id])) {
+        $post_body = $_POST['post_body'];
+        $post_body = mysqli_escape_string($con, $post_body);
+        $date_time_now = date("Y-m-d H:i:s");
+        $insert_post = mysqli_query($con, "INSERT INTO comments(post_body, posted_by, posted_to, date_added, removed, post_id) VALUES ('$post_body','$userLoggedIn','$posted_to','$date_time_now','no','$post_id'");
+        echo "<p>Comment posted!</p>";
+    }
+
+    ?>
+    <form action="comment_frame.php?post_id=<?= $post_id ?>" id="comment_form" name="postComment<?= $post_id ?>" method="POST" >
+        <textarea name="post_body"></textarea>
+        <input type="submit" name="postComment<?= $post_id ?>" value="Post">
+    </form>
+
+    <!-- Load comments -->
+</body>
+</html>
