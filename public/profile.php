ef558cbc1cd4f635fcddde204c81417e0e3ca729
Kirill Koldrysh <kirill.koldrysh@redbit.cz>
2020-04-23 15:13:46 +0200

    fix bugs

--- public/profile.php
+++ public/profile.php
@@ -101,7 +101,7 @@ if (isset($_POST['respond_request'])) {
 
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary" name="post_button" id="submit_profile_post">Save changes</button>
+        <button type="button" class="btn btn-primary" name="post_button" id="submit_profile_post">Post</button>
       </div>
     </div>
   </div>
