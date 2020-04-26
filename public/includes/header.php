117177794944770631c0e93bf900ee96e8a82601
BadKoldrysh <ohmygodkir17@gmail.com>
2020-04-26 21:46:56 +0200

    create upload script that upload and crop image for account photo

--- public/includes/header.php
+++ public/includes/header.php
@@ -23,10 +23,13 @@ if (isset($_SESSION['username'])) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="/../assets/js/bootstrap.js"></script>
+    <script src="/../assets/js/jcrop_bits.js"></script>
+    <script src="/../assets/js/jquery.Jcrop.js"></script>
     <script src="/../assets/js/bootbox.min.js"></script>
     <script src="/../assets/js/demo.js"></script>
 
     <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="/../assets/css/jquery.Jcrop.css">
     <link rel="stylesheet" type="text/css" href="/../assets/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="/../assets/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
