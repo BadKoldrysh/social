2e91b6bf14af3fa6e9d63911615e2cf090d0ea5a
BadKoldrysh <ohmygodkir17@gmail.com>
2020-05-01 20:47:16 +0200

    add method for getting all conversations

--- public/messages.php
+++ public/messages.php
@@ -81,4 +81,13 @@ if (isset($_POST['post_message'])) {
         const div = document.getElementById("scroll_messages");
         div.scrollTop = div.scrollHeight;
     </script>
+
+    <div class="user_details column" id="conversations">
+            <h4>Conversations</h4>
+            <div class="loaded_conversations">
+                <?= $message_obj->getConvos(); ?>
+            </div>
+            <br />
+            <a href="messages.php?u=new">New Message</a>
+    </div>
 </div>
